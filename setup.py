ca55cc24f523aaa7cf2df29a25df566c8815fd60
mj.ki <mj.ki@aligo.ai>
2023-02-27 12:53:04 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(exclude=['demo*']),
     install_requires=[
         'django>=3.5',
-        'djangorestframework>=3.0'
+        'djangorestframework>=3.0',
         'django-appconf>=1.0.0',
         'pytrends>=4.8.0',
         'django_action_reservation @ git+https://github.com/aligo-acp/django-action-reservation.git#egg=django_action_reservation',
